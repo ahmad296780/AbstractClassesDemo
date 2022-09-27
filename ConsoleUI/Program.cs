84a8ab88c94348b03e7dfe960136589d01c44306
ahmad <bha296780@gmail.com>
2022-09-27 14:30:06 -0400

    final

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -26,27 +26,47 @@ namespace ConsoleUI
 
             /* 
              * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
              * Provide the implementations for the abstract methods
              * Only in the Motorcycle class will you override the virtual drive method
             */
 
             // Create a list of Vehicle called vehicles
-
+            List<Vehicle> vehicles = new List<Vehicle>();
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            Car car1 = new Car () { HasTrunk = true, Make = "honda", Year = "2020", Model = "Accord" };
+
+            Motorcycle yamaha = new Motorcycle () { HasSideCart = true, Make = "yamaha", Year = "2020", Model = "rx1" };
+            Vehicle acura = new Car () { HasTrunk = true, Make = "honda", Year = "2021", Model = "mdx" };
+            Vehicle suzuki = new Motorcycle () { HasSideCart = true, Make = "suzuki", Year = "2019", Model = "gtxr" };
 
             /*
-             * Add the 4 vehicles to the list
-             * Using a foreach loop iterate over each of the properties
-             */
+         * Add the 4 vehicles to the list
+         
+         * Using a foreach loop iterate over each of the properties
+         */
+            vehicles.Add(car1);
+            vehicles.Add(yamaha);
+            vehicles.Add(acura);
+            vehicles.Add(suzuki);
+           // Console.WriteLine(vehicles.Count);
+            foreach (var item in vehicles)
+            {
+                Console.WriteLine($"{item.Year},{item.Make},{item.Model}");
+            }
 
+            Console.WriteLine("---------");
             // Call each of the drive methods for one car and one motorcycle
-
-            #endregion            
+            car1.DriveAbstract();
+            yamaha.DriveAbstract();
+            car1.DriveVirtual();
+            yamaha.DriveVirtual();
+            
+            #endregion
             Console.ReadLine();
         }
     }
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public string Year { get; set; } = "2000";
+        public string Make { get; set; } = "Toyota";
+        public string Model { get; set; } = "Camry";
+
+        public abstract void DriveAbstract();
+        public void DriveVirtual()
+        {
+            Console.WriteLine("drive virtual");
+        }
+    }
+   public class Car : Vehicle
+    {
+        public bool HasTrunk { get; set; }
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("driving car abstract");
+        }
+    }
+    public class Motorcycle : Vehicle
+    {
+       public bool HasSideCart { get; set; }
+       public  override void DriveAbstract()
+        {
+            Console.WriteLine("driving bike abstract");
+        }
+    }
+}
